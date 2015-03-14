06e2fbc85ac1296e5ac147d215e8d4942d8f7f68
Steve <b0rg3r@gmail.com>
2015-03-14 03:21:07 -0500

    Refactoring, more details. - Improved relative links related to the root directory. - Created CSS folder for global.css file.

--- contact/index.php
+++ contact/index.php
@@ -5,10 +5,10 @@
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
     <title>Miacro Power Clan - MPC Gaming.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <script src="../bootstrap/js/jquery-1.11.2.js" type="text/jscript"></script>
-    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet"/>
-    <script src="../bootstrap/js/bootstrap.js" type="text/jscript"></script>
-    <link href="../index.css" rel="stylesheet"/><!--index only-->
+    <script src="/bootstrap/js/jquery-1.11.2.js" type="text/jscript"></script>
+    <link href="/bootstrap/css/bootstrap.css" rel="stylesheet"/>
+    <script src="/bootstrap/js/bootstrap.js" type="text/jscript"></script>
+	<link href="/includes/css/global.css" rel="stylesheet"/>
     <meta name="keywords" content="MPC, SC2, MPCGaming.com"/>
     <meta name="description" content="SC2 MPC Gaming. Tournaments, Clan Wars, Teaching, Training, Coaching, Community Clan, Ladders, Clan Ranking" />
 </head>

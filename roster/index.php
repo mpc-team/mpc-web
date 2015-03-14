25270edef3b786f3d5ff5039796365e828113403
Steve <b0rg3r@gmail.com>
2015-03-14 04:25:49 -0500

    Refactored HTML headers.

--- roster/index.php
+++ roster/index.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
+<?php
+
+	$ROOT = '..';
+	
+	include_once($ROOT . '/includes/pathdir.php');
+	include_once($ROOT . PathDir::$NAVBAR);
+	include_once($ROOT . PathDir::$FOOTER);
+	
+	$JQUERY = $ROOT . PathDir::$JQUERY;
+	$BS_CSS = $ROOT . PathDir::$BS_CSS;
+	$BS_JS  = $ROOT . PathDir::$BS_JS;
+	$CSS_GLOBAL = $ROOT . PathDir::$CSS . 'global.css';
+ ?>
+
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>Miacro Power Clan - MPC Gaming.com</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<script src="/bootstrap/js/jquery-1.11.2.js" type="text/jscript"></script>
-	<link href="/bootstrap/css/bootstrap.css" rel="stylesheet"/>
-	<script src="/bootstrap/js/bootstrap.js" type="text/jscript"></script>
-	<link href="/includes/css/global.css" rel="stylesheet"/>
+	<?php
+		$print = 
+	   "<script src='{$JQUERY}' type='text/jscript'></script>
+		<link href='{$BS_CSS}' rel='stylesheet'/>
+		<script src='{$BS_JS}' type='text/jscript'></script>
+		<link href='{$CSS_GLOBAL}' rel='stylesheet'/>";
+		echo ($print);
+	 ?>
 	<meta name="keywords" content="MPC, SC2, MPCGaming.com"/>
 	<meta name="description" content="SC2 MPC Gaming. Tournaments, Clan Wars, Teaching, Training, Coaching, Community Clan, Ladders, Clan Ranking" />
 </head>
 
 <body>
-	
-	<?php
-		include_once('../includes/navbar.php');
-		include_once('../includes/footer.php');
-	 ?>
-
 	<div class="container-fluid">
 		
 		<?php PrintNavbar("roster"); ?>

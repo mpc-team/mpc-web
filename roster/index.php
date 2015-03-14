39c284fcf06fd61480211efda15e71bd7fb8cebc
AcidSnake <keatingc88@gmail.com>
2015-03-14 04:30:14 -0500

    php shit

--- roster/index.php
+++ roster/index.php
@@ -5,10 +5,11 @@
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>Miacro Power Clan - MPC Gaming.com</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<script src="/bootstrap/js/jquery-1.11.2.js" type="text/jscript"></script>
-	<link href="/bootstrap/css/bootstrap.css" rel="stylesheet"/>
-	<script src="/bootstrap/js/bootstrap.js" type="text/jscript"></script>
-	<link href="/includes/css/global.css" rel="stylesheet"/>
+	<script src="../bootstrap/js/jquery-1.11.2.js" type="text/jscript"></script>
+	<link href="../bootstrap/css/bootstrap.css" rel="stylesheet"/>
+	<script src="../bootstrap/js/bootstrap.js" type="text/jscript"></script>
+	<link href="../includes/css/global.css" rel="stylesheet"/>
+	<link href="../rostercss/roster.css" rel="stylesheet"/>
 	<meta name="keywords" content="MPC, SC2, MPCGaming.com"/>
 	<meta name="description" content="SC2 MPC Gaming. Tournaments, Clan Wars, Teaching, Training, Coaching, Community Clan, Ladders, Clan Ranking" />
 </head>
@@ -27,23 +28,30 @@
 	</div>
 
 	   <div class="row">
-            <div class="col-xs-3">
-                    <div class="dropdown">
-                    <button class="btn btn-success dropdown-toggle" type="button" id="searchbypub" data-toggle="dropdown" aria-expanded="true">
-                    Search
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu-right" role="menu" arialabelledby="searchbypub">
-                        <li role="presentation" class="dropdown-header">Search By:</li>
-                        <li role="presentation" class="divider"></li>
-                        <li role="presentation"><a href="#" role="menuitem" tabindex="-1">Game</a></li>
-                        <li role="presentation"><a href="#" role="menuitem" tabindex="-1">MPC ID</a></li>
-                        <li role="presentation"><a href="#" role="menuitem" tabindex="-1">Email Address</a></li>
-                        <li role="presentation"><a href="#" role="menuitem" tabindex="-1">Clan Name/ID</a></li>
-                    </ul>
-                </div>    
-            </div>
-            <div class="col-xs-9">
+           
+            <div class="col-xs-3 well">
+                <?php include('searchbtn.php'); ?>
+                <br />
+                <?php switch ($dynamicsearch) {
+                case "games": 
+                    echo "connect to games table";
+                    break;
+                case "mpcid":
+                    echo "connect to mpc id table";
+                    break;
+                case "emailad":
+                    echo "connect to email address table";
+                    break;
+                case "clan-name-id":
+                    echo "connect to clan table";
+                    break;
+                case default;
+                    echo "connect to default table";
+
+                ?>            
+           </div>
+            <div class="col-xs-9 well">
+                <?php include('dynamicinfo.php'); ?>
             </div>
         </div>
 	

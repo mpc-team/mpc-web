b6d3b4e518eaa4b13d84d3a2fe6c8d15d1045acb
Steve <b0rg3r@gmail.com>
2015-03-14 15:56:51 -0500

    Fixed deletion of login/index.php.

---
+++ login/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+	$ROOT = '..';
+	include_once($ROOT . '/includes/pathdir.php');
+	include_once($ROOT . PathDir::$NAVBAR);
+	include_once($ROOT . PathDir::$FOOTER);
+	include_once($ROOT . PathDir::$LOGINFORM);
+	
+	$JQUERY = $ROOT . PathDir::$JQUERY;
+	$BS_CSS = $ROOT . PathDir::$BS_CSS;
+	$BS_JS  = $ROOT . PathDir::$BS_JS;
+	$CSS_GLOBAL = $ROOT . PathDir::$CSS . 'global.css';
+ ?>
+
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<title>Miacro Power Clan - MPC Gaming.com</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<?php
+		$print = 
+	   "<script src='{$JQUERY}' type='text/jscript'></script>
+		<link href='{$BS_CSS}' rel='stylesheet'/>
+		<script src='{$BS_JS}' type='text/jscript'></script>
+		<link href='{$CSS_GLOBAL}' rel='stylesheet'/>";
+		echo ($print);
+	 ?>
+	<meta name="keywords" content="MPC, SC2, MPCGaming.com"/>
+	<meta name="description" content="SC2 MPC Gaming. Tournaments, Clan Wars, Teaching, Training, Coaching, Community Clan, Ladders, Clan Ranking" />
+</head>
+<body>
+
+	<div class="container-fluid">
+	
+		<?php PrintNavbar("login", $ROOT); ?>
+		
+	</div>
+		
+	<div class="container">
+		<div class="row">
+		
+			<div class="col-md-8">
+				<img src="../pics/mpclogo.png" class="img-responsive" alt="mpclogo.png"/>
+				<?php PrintLoginForm($ROOT); ?> 
+			</div>
+				
+		</div>
+	</div>
+
+	<div class="container-fluid">
+	
+		<?php PrintFooter(); ?>
+		
+	</div>
+
+</body>
+
+</html>

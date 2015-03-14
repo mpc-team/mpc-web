31b06c9e5c1794870fe862db425a7323d31b95ba
Steve <b0rg3r@gmail.com>
2015-03-13 22:38:43 -0500

    Revert "Updated 'roster.php'"
    This reverts commit a070aec84b5742a7fd5147996f22ee346f09e3da.

--- roster/index.php
+++ roster/index.php
@@ -25,58 +25,9 @@
 	</div>
 
 	<div class="container">
-	   <div class="row">
-            <center>
-            <img src="../pics/photoshop/grn13.jpg" style="height: 120px; width: 300px; border: solid; border-top-width: 10px; border-bottom-width: 10px; border-right-width: 10px; border-left-width: 10px; border-color: purple; " class="img-reponsive text-center" alt="#" />
-            </center><br />
-        </div>
-        <div class="row">
-            <div class="col-xs-4 well">
-                <div class="page-header text-center">
-                    <h4>Legacy Members(Founders)</h4><!--
-                <br />
-                    <h4>(Moderators)</h4>
-                <br />
-                    <h4>(Commanders)</h4>
-                <br />
-                    <h4>(Officers-elite)</h4>
-                <br />
-                    <h4>(Officers)</h4>
-                <br />
-                    <h4>(Member Elites)</h4>
-                <br />
-                    <h4>(Members)</h4>
-                <br />
-                    <h4>(Recruits)</h4>
-                <br />
-                    <h4>(initiates /applicants)</h4>--><br />
-                    <h4>Legacy Member Briefing (CLICKABLE)</h4>
-                </div>
-            </div>
-            <div class="col-xs-4 well">
-                <div class="page-header text-center">
-                    <h4>Pic/Photo/Image</h4>
-                    <h4>Quote/Comments/Nick Names</h4>
-                    </div>
-            </div>
-            <div class="col-xs-4 well">
-                <div class="page-header text-center">
-                    <h4>Listtype: Game</h4>
-                    <h4>Game Type Information / Rank</h4>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-4 well">
-            </div>
-            <div class="col-xs-4 well">
-                <h4>Information on selected db, member info, comments</h4>
-            </div>
-            <div class="col-xs-4 well">
-                <h4>Database Selector</h4>
-            </div>
-        </div>
+	
 	</div>
+	
 	<div class="container-fluid">
 		
 		<?php include('../includes/footer.php'); ?>

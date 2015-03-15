2e092eb3b3ace6d503db547a0737c90e6dcfd615
Steve <b0rg3r@gmail.com>
2015-03-14 20:05:06 -0500

    Added PHP for including HTML headers. Minimized the amount of content that is in the header section for both PHP and HTML. Added PHP functions for including CSS/JS files from their directories in PathDir.php, as well as other common header stuff.

---
+++ includes/htmlheader.php
@@ -0,0 +1,11 @@
+<?php
+
+function PrintJavaScriptResource($path) {
+	echo "<script src='$path' type='text/javascript'></script>";
+}
+
+function PrintStyleResource($path) {
+	echo "<link href='$path' rel='stylesheet'/>";
+}
+
+ ?>
--- includes/pathdir.php
+++ includes/pathdir.php
@@ -11,11 +11,12 @@
 
 class PathDir 
 {	
-	# Page Elements
+	# PHP Scripts
 	public static $FOOTER = '/includes/footer.php';
 	public static $NAVBAR = '/includes/navbar.php';
 	public static $LOGINFORM = '/login/form.php';
 	public static $SIGNUPFORM = '/login/signup/form.php';
+	public static $HTMLHEADER = '/includes/htmlheader.php';
 	
 	# Directories
 	public static $CSS = '/includes/css/';
@@ -26,9 +27,33 @@ class PathDir
 	public static $BS_JS = '/bootstrap/js/bootstrap.js';
 	public static $JQUERY = '/bootstrap/js/jquery-1.11.2.js';
 	
+	
 	public static $PATHDIR = '/includes/pathdir.php';
 	
+	
+	# ---- Utility Functions -------------------------------
+	
+	public static function GetJQueryPath($root) {
+		return $root . self::$JQUERY;
+	}
+	
+	public static function GetBootstrapCSSPath($root) {
+		return $root . self::$BS_CSS;
+	}
+	
+	public static function GetBootstrapJSPath($root) {
+		return $root . self::$BS_JS;
+	}
+	
+	public static function GetCSSPath($root, $cssName) {
+		return $root . self::$CSS . $cssName;
+	}
+	
+	public static function GetJSPath($root, $jsName) {
+		return $root . self::$JS . $jsName;
+	}
 }
+
 
 ?>
 

eb01467808c5bd1fbac17cb0023de18e2948aff0
Steve <b0rg3r@gmail.com>
2015-03-14 21:10:08 -0500

    Added some DB utilities.

---
+++ includes/db/util.php
@@ -0,0 +1,39 @@
+<?php
+
+#
+#
+# 	Database Functions
+#
+#
+
+class dbutil
+{
+	public $host = '';
+	public $user = '';
+	public $pass = '';
+	public $name = '';
+	public $mysqli = null;
+
+	public function __construct($host, $user, $pass, $dbname)
+	{
+		$this->host = $host;
+		$this->user = $user;
+		$this->pass = $pass;
+		$this->name = $dbname;
+	}
+	
+	public function connect()
+	{
+		$this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->name);
+		if ($this->mysqli->connect_errno) {
+			echo '<h2>Failure:</h2><em>' . $this->mysqli->connect_error . '</em>';
+			return false;
+		} 
+		echo '<h2>Successful Connection Established to Database</h2>';
+		return true;
+	}
+	
+}
+
+
+ ?>
--- login/signup/signup-complete.php
+++ login/signup/signup-complete.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
+ 
+<head>
 <?php
 
 	$ROOT = '../..';
@@ -8,10 +10,10 @@
 	include_once($ROOT . '/includes/pathdir.php');
 	include_once($ROOT . PathDir::$FOOTER);
 	include_once($ROOT . PathDir::$HTMLHEADER);
+	include_once($ROOT . PathDir::$DB_UTILITY);
+	include_once($ROOT . PathDir::$DB_INFO);
 	
  ?>
- 
-<head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>Miacro Power Clan - MPC Gaming.com</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -31,13 +33,20 @@
 		<div class="page-header text-center">
 			<h1>Signup Processing...</h1>
 		</div>
-
-
 		
 		<div class="text-center">
+		
+<?php
+	$dbhandle = new dbutil(dbinfo::$HOST, dbinfo::$USER, dbinfo::$PASS, dbinfo::$NAME);
+	$dbhandle->connect();
+	
+ ?>
+		
 			<p> Hello 			<?php echo $_POST["alias"]; ?> </p>
 			<p> Email Address:  <?php echo $_POST["email"]; ?> </p>
 			<p> Password:  		<?php echo $_POST["password"]; ?> </p>
+		
+			
 		</div>
 
 		<?php PrintFooter(); ?>

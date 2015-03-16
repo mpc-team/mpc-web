6c441745827f0e44653041875565b810b38cd0c5
Steve <b0rg3r@gmail.com>
2015-03-16 18:45:11 -0500

    Sidebar modification for Roster.

--- roster/includes/sidebar.php
+++ roster/includes/sidebar.php
@@ -1,13 +1,14 @@
 <?php 
 
-function PrintSidebar ($highlight, $root_path) {	
+function PrintSidebar ($highlight, $root) {	
+	$MAINPAGE = $root . '/roster/index.php';
 	$ACTIVE = "class='active'";
 	echo <<<EOD
 		<div id="sidebar-wrapper">
 			<ul class="sidebar-nav"> 
 EOD;
 	$class = ($highlight == "members") ? $ACTIVE : "";
-	echo ("<li {$class}><a href='#'>Members</a></li> </ul></div>");
+	echo ("<li {$class}><a href='{$MAINPAGE}'>Members</a></li> </ul></div>");
 }
 
  ?>

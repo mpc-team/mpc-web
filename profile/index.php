33bf6a8e3414ece96666b23b205f9eb9cf3d9c4b
AcidSnake <keatingc88@gmail.com>
2015-03-17 03:27:43 -0500

    updates on the profile

--- profile/index.php
+++ profile/index.php
@@ -45,6 +45,7 @@
 	PrintJavaScriptResource( PathDir::GetBootstrapJSPath($ROOT) );
 	PrintStyleResource( PathDir::GetCSSPath($ROOT, 'global.css') );
  ?>
+    <link href="includes/profile.css" rel="stylesheet" />
 	<meta name="keywords" content="MPC, SC2, MPCGaming.com"/>
 	<meta name="description" content="SC2 MPC Gaming. Tournaments, Clan Wars, Teaching, Training, Coaching, Community Clan, Ladders, Clan Ranking" />
 </head>
@@ -60,66 +61,141 @@
 				echo "<p>{$user}</p>";
 			 ?>
 		</div>
-        <div class="well" id="userprofile-edit-form">
+        <div class="well">
             <form role="form">
                 <div class="row">
-									<div class="page-header pull-left">
-										<?php echo "<h3>Update {$alias}'s Account:</h3></br>"; ?>
-									</div>
+                        <div class="page-header">
+                            <?php echo "<h3>Update {$alias}'s Account:</h3></br>"; ?>
+                        </div>
                     <div class="col-xs-6">
-											<div class="page-header pull-left">
-												<h3><b>Manditory for all Accounts</b></h3>
-											</div>
-											<div class="form-group">
+                        <div class="form-group">
                         <label for="alias-profile"><p>Profile Name:</p></label>
                         <input type="text" class="form-control" id="alias-profile" title="Change Profile Name" placeholder="Change Name">
                       </div>
-											</br>
+				        <br />
                       <div class="form-group">
-												<label for="password-profile"><p>New Password:</p></label>
+				        <label for="password-profile"><p>New Password:</p></label>
                         <input type="text" class="form-control" id="password-profile" title="Change Profile Password" placeholder="Change Password">
+                          <br />
                         <label for="password-profile"><p>Re-Enter Password:</p></label>
                         <input type="text" class="form-control" id="password-profile" title="Re-Enter Password" placeholder="Re-Enter Password">
                       </div>
-                      </br>
+                      <br />
                       <div class="form-group">
-												<label for="email-profile"><p>Change Email:</p></label>
+				        <label for="email-profile"><p>Change Email:</p></label>
                         <input type="email" class="form-control" id="email-profile" title="Change Profile Email" placeholder="Change Email">
                        </div>
                     </div>
                     <div class="col-xs-6">
-                        <div class="page-header pull-right"><h3><i>Optional!</i></h3></div>
-                        <div class="radio">
-                            <label><input type="radio" name="mr-profile" value="mr"><p>MR.</p></label>
-                            <label><input type="radio" name="mrs-profile" value="mrs"><p>MRS.</p></label>
-                            <label><input type="radio" name="ms-profile" value="ms"><p>MS.</p></label>
-                        </div>
                         <div class="form-group">
-                            <label for="fname-profile"><p>First Name</p></label>
+                            <label for="fname-profile"><p>First Name:</p></label>
                             <input type="text" class="form-control" id="fname-profile" title="Enter First Name" placeholder="First Name">
                         </div>
                         <br />
                         <div class="form-group">
-                            <label for="lname-profile"><p>Last Name</p></label>
+                            <label for="lname-profile"><p>Last Name:</p></label>
                             <input type="text" class="form-control" id="lname-profile" title="Enter Last Name" placeholder="Last Name">
                         </div>
                         <div class="form-group">
-                            <div id="spend_filter" class="btn-group">
-                                <a class="btn btn-small dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i class="icon-filter"></i>
-                                Filter By
-                                <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-																	<li><a href="#">Test</a></li>
-																	<li><a href="#">Test 2</a></li>
-																	<li><a href="#">Test 3</a></li>
-																	<li><a href="#">Test 4</a></li>
-																</ul>
-                            </div>
+                            <label for="location-profile"><p>Location:</p></label>
+                            <input type="text" class="form-control" id="location-profile" title="Enter Location" placeholder="City, State, Province, Country">
+                        </div>
+                        <div class="form-group">
+                            <label for="comment-profile">Comment:</label>
+                            <textarea class="form-control" rows="1" id="comment-profile"></textarea>
+                    </div>
+                    </div>
+            </div>
+                        <br />
+            <div class="well">
+                    <div class="page-header"><h3>Games Played with MPC:</h3></div><small>Use Checkboxes to Share Alias</small><br /><small>X = Enable Setting</small>
+                    <div class="row">
+                    <div class="col-xs-6">
+                        <div class="checkbox-inline">
+                            <label><input type="checkbox" name="sc2hot-profile"><p>Starcraft 2 Heart of the Swarm</p></label>
+                        </div>
+                        <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Character Code">
+                        </div>
+                        <div class="checkbox-inline">
+                            <label><input type="checkbox" name="wow-profile"><p>World of WarCraft</p></label>
+                        </div>
+                        <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Character Code">
+                        </div>
+                        <div class="checkbox-inline">
+                            <label><input type="checkbox" name="hearthstone-profile"><p>Hearth Stone</p></label>
+                        </div>
+                        <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Character Code">
+                        </div>
+                        <br />
+                        <div class="input-group">
+                          <span class="input-group-addon" id="sizing-addon2">Steam</span>
+                          <input type="text" class="form-control" placeholder="Steam Name" aria-describedby="sizing-addon2">
+                        </div>
+                        <div class="input-group">
+                          <span class="input-group-addon" id="sizing-addon2">Battle.net</span>
+                          <input type="email" class="form-control" placeholder="Email" aria-describedby="sizing-addon2">
+                        </div>
+                        <div class="input-group">
+                          <span class="input-group-addon" id="sizing-addon2">Origin</span>
+                          <input type="email" class="form-control" placeholder="Email" aria-describedby="sizing-addon2">
                         </div>
                     </div>
+                    <div class="col-xs-6">
+                        <div class="checkbox-inline">
+                            <label><input type="checkbox" name="csgo-profile"><p>Counter Strike Global Offensive</p></label>
+                        </div>
+                        <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Character Code">
+                        </div>
+                        <div class="checkbox-inline">
+                            <label><input type="checkbox" name="herosofthestorm-profile"><p>Heroes of the Storm</p></label>
+                        </div>
+                        <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Character Code">
+                        </div>
+                        <div class="checkbox-inline">
+                            <label><input type="checkbox" name="leagueoflegends-profile"><p>League of Legends</p></label>
+                        </div>
+                        <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Character Code">
+                        </div>
+                        <div class="checkbox-inline">
+                        <label><input type="checkbox" name="mgsvonline-profile" disabled><p>Metal Gear Solid V: Multiplayer</p></label>
+                        </div>
+                        <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Character Code">
+                        </div>
+                    </div>
+                    </div>
                 </div>
+            <br />
+            <div class="well">
+                <div class="page-header"><h3>Preference Settings</h3></div><small>X = enable Setting</small>
+                <div class="row">
+                <div class="col-xs-6">
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="display-email-profile"><p>Share Email</p></label>
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="display-games-profile"><p>Share Games</p></label>
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="display-location-profile"><p>Share Location</p></label>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="display-gametag-profile"><p>Share In-Game Character Codes/Alias</p></label>
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="display-gametag-profile"><p>Share Steam/Battle.net/Origin Accounts</p></label>
+                    </div>
+                </div>
+                </div>
+            </div>
             </form>
         </div>
 	</div>

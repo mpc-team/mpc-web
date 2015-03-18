605fb9f6f9776d7faa5064201295002891999f1c
Steve <b0rg3r@gmail.com>
2015-03-18 00:42:09 -0500

    Nothing.

--- gaming/index.php
+++ gaming/index.php
@@ -33,7 +33,9 @@
 			<div class="page-header text-center">
 				<h1>What do we play?</h1>
 			</div>
-                <small><p>Mpc is a multi-level gaming clan that specializes on one main concept... Team. We would like to develope a community that engages both the players, and the games they play. We try to provide the essential fundamentals of gaming, and that includes: ventrilo, website domain, a foundation of solid core members, training, stream zone, and more. It's a friendly environment for all ages, and disrespect to either members, or to public community will not be tolerated.</p></small>
+			<small>
+				<p>MPC is a multi-level gaming clan that specializes on one main concept... Team. We would like to develope a community that engages both the players, and the games they play. We try to provide the essential fundamentals of gaming, and that includes: ventrilo, website domain, a foundation of solid core members, training, stream zone, and more. It's a friendly environment for all ages, and disrespect to either members, or to public community will not be tolerated.</p>
+			</small>
 			<div class="col-xs-3">
 				<img src="../pics/starcraft2blizzardlogo.PNG" id="sc2logo-gaming" alt="starcraft2blizzardlogo.PNG" class="img-reponsive" style="max-width: 75%; max-height: 75%; margin-top: 15px;" />
 			</div>
@@ -41,16 +43,18 @@
 				<img src="../pics/cs-go-logo.png" id="csgologo-img" alt="cs-go-logo.png" class="img-reponsive" style="max-width: 100%; max-height: 100%; margin-top: 15px;" />
 			</div>
 			<div class="col-xs-3" style="margin-bottom: 45px;">
-					<img src="../pics/blizzardwowlogo.PNG" id="blizzardwowlogo-img" alt="blizzardwowlogo.PNG" class="img-responsive" />
+				<img src="../pics/blizzardwowlogo.PNG" id="blizzardwowlogo-img" alt="blizzardwowlogo.PNG" class="img-responsive" />
 			</div>
 			<div class="col-xs-3">
 				<img src="../pics/leagueoflegendslogo.png" id="leagueofleagends-img" alt="leagueoflegendslogos.png" class="img-responsive" style="margin-top: 5px;" />
 			</div>
 		</div>
-        <div class="row">
-            <div class="page-header left-pull"><h3>How do we communicate?</h3></div>
-            <a href="http://www.ventrilo.com/status.php?hostname=lead.typefrag.com&port=7920">ventrilo</a>
-        </div>
+		<div class="row">
+			<div class="page-header left-pull">
+				<h3>How do we communicate?</h3>
+			</div>
+			<a href="http://www.ventrilo.com/status.php?hostname=lead.typefrag.com&port=7920">ventrilo</a>
+		</div>
 	</div>
 	<div class="container-fluid">
 		<?php PrintFooter($ROOT); ?>

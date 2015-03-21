51fc2c88235c53031a56dd0acb4c01ceb8cc8b54
Steve <b0rg3r@gmail.com>
2015-03-21 01:28:33 -0500

    Forum navbar updates.

--- forum/includes/navbar.php
+++ forum/includes/navbar.php
@@ -1,12 +1,13 @@
 <?php 
 function PrintForumNavbar($highlight,$root){
+	$home=$root."/forum/";
 	$ACTIVE="class='active'";
 	echo <<<EOD
 		<nav role="navigation" class="navbar navbar-default navbar-static-top navbar-fixed-top">
 			<ul class="nav navbar-nav">
 EOD;
 	$active=($highlight == "forum") ? $ACTIVE : "";
-	echo "<li {$active}><a href='..'>Forum</a></li>";
+	echo "<li {$active}><a href='{$home}'>Forum</a></li>";
 	$active="";
 	echo "<li {$active}><a href='#'>General Testing</a></li>";
 	echo <<<EOD

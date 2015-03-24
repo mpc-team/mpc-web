1a51c9aca2156f34cc8e3449646cd2d15b891a6e
Steve <b0rg3r@gmail.com>
2015-03-23 22:09:58 -0500

    Redid redirect.

--- forum/message/delete.php
+++ forum/message/delete.php
@@ -19,7 +19,7 @@
 			$del=DeleteMessage($cid,$ctag,$tid,$ttag,$_POST["message"]);
 		}	
 	}
-	//header($header);
+	header($header);
  ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"> 

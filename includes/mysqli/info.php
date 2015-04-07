59e1ef3c12374f7b7424a0257b8c80f1b4cee1bd
Steve <b0rg3r@gmail.com>
2015-04-06 23:53:50 -0500

    lets see if this updates

--- includes/mysqli/info.php
+++ includes/mysqli/info.php
@@ -1,7 +1,7 @@
 <?php
 class dbinfo {
 	public static $HOST = "clanmpc.db.9825370.hostedresource.com";
-	public static $PASS = "W3W!NGames";
+	public static $PASS = "semaGN!W3W";
 	public static $USER = "clanmpc";
 	public static $NAME = "clanmpc";
 }
